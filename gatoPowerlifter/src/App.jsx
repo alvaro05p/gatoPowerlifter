a1534cd9f195e9b115cc94aa2e158065d49093c2
alvaro05p <alvaro05pomg@gmail.com>
2025-09-27 05:51:41 +0200

    inputs

--- gatoPowerlifter/src/App.jsx
+++ gatoPowerlifter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Ejercicio } from './Ejercicio'
 
@@ -8,17 +6,24 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Gato powerlifter</h1>
-      <h2>Gestiona tus entrenes de forma gratuita</h2>
-      <section className="homeButtons">
-        <button>Añadir series</button>
-        <button>Registro</button>
-      </section>
-      <Ejercicio nombre="Peso muerto"></Ejercicio>
-      <Ejercicio nombre="Press Banca"></Ejercicio>
-      <Ejercicio nombre="Sentadilla"></Ejercicio>
-    </>
+    <div className="container text-center py-5">
+      <h1 className="display-4 fw-bold mb-3">Gato Powerlifter</h1>
+      <h2 className="h4 mb-4">
+        Gestiona tus entrenes de forma gratuita
+      </h2>
+      
+      <div className="row g-4">
+        <div className="col-md-4">
+          <Ejercicio nombre="Peso muerto" />
+        </div>
+        <div className="col-md-4">
+          <Ejercicio nombre="Press Banca" />
+        </div>
+        <div className="col-md-4">
+          <Ejercicio nombre="Sentadilla" />
+        </div>
+      </div>
+    </div>
   )
 }
 
